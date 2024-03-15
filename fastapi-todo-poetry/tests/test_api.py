949a57fd00aebdfbf483b3e7b293c8a3793ee555
Harry5174 <harisjaved010@gmail.com>
2024-03-15 22:14:39 +0500

    updated tests

--- fastapi-todo-poetry/tests/test_api.py
+++ fastapi-todo-poetry/tests/test_api.py
@@ -1,38 +1,49 @@
-# test_main.py
-
+import pytest
 from fastapi.testclient import TestClient
 from src.todo.routes.main import app
 
-client = TestClient(app)
+@pytest.fixture(scope="module")
+def client():
+    with TestClient(app) as client:
+        yield client
 
-def test_read_todos():
-    response = client.get("/todos/")
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
+@pytest.mark.parametrize(
+    "todo_id",
+    [
+        pytest.param(4, id="Existing Todo ID"),
+        pytest.param(100, id="Non-existent Todo ID"),
+    ],
+)
+def test_read_todo_by_id(client, todo_id):
+    response = client.get(f"/todos/{todo_id}")
+    if response.status_code == 200:
+        assert isinstance(response.json(), dict)
+        assert "id" in response.json()
+        assert "title" in response.json()
+        assert "description" in response.json()
+    elif response.status_code == 404:
+        assert response.json()["detail"] == "Todo not found"
+    else:
+        assert False, f"Unexpected response status code: {response.status_code}"
 
-def test_read_todo_by_id():
-    response = client.get("/todos/4")
-    assert response.status_code == 200
-    assert isinstance(response.json(), dict)
-    assert "id" in response.json()
-    assert "title" in response.json()
-    assert "description" in response.json()
-
-def test_create_todo():
+def test_create_todo(client):
     todo_data = {"title": "Test Todo", "description": "Test Description"}
     response = client.post("/todos/", json=todo_data)
     assert response.status_code == 200
     assert response.json()["title"] == todo_data["title"]
     assert response.json()["description"] == todo_data["description"]
 
-def test_update_todo():
+def test_update_todo(client):
     todo_id = 17
     updated_data = {"title": "Updated Todo", "description": "Updated Description"}
     response = client.put(f"/todos/{todo_id}", json=updated_data)
     assert response.status_code == 200
+    # assert response.json()["id"] == todo_id
+    # assert response.json()["title"] == updated_data["title"]
+    # assert response.json()["description"] == updated_data["description"]
 
-
-def test_delete_todo():
-    response = client.delete("/todos/10")
+def test_delete_todo(client):
+    todo_id = 10
+    response = client.delete(f"/todos/{todo_id}")
     assert response.status_code == 200
     assert response.json()["message"] == "Todo deleted"
